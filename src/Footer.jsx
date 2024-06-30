0b8f60c070a1aaa244587c11b33823c47de41d1e
Kenneth Kammersgaard <kennethkammersgaard@gmail.com>
2024-06-30 15:32:13 +0200

    Fjernet facebook login

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import FacebookLoginComponent from './FacebookLoginComponent';
 
 function Footer({ user, setUser }) {
   return (
     <footer style={{ backgroundColor: '#f0f0f0', textAlign: 'center', padding: '10px 0' }}>
       <Link to="/terms" style={{ marginRight: '10px' }}>Vilkår for brug</Link>
       <Link to="/privacy">Privatlivspolitik</Link>
-      {!user && (
-        <div style={{ position: 'absolute', bottom: '10px', right: '10px' }}>
-          <FacebookLoginComponent setUser={setUser} />
-        </div>
-      )}
     </footer>
   );
 }
